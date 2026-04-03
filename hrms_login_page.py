3463adef6ad6c8fec2800d940d4205b81d825eff
PonduruManikantha92 <neelamanikantha@gmail.com>
2026-04-03 18:37:28 +0530

    Refactor HRMS login page test for better readability

--- hrms_login_page.py
+++ hrms_login_page.py
@@ -1,32 +1,25 @@
 import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.ie.webdriver import WebDriver
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-
-def hrms_login_page():
+def test_hrms_login_page():
     driver = webdriver.Chrome()
 
     driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     driver.maximize_window()
-    xpath_for_username = "//input[@placeholder='Username']"
-    xpath_for_password = "//input[@placeholder='Password']"
-    xpath_for_submit = "//button[@type='submit']"
 
     wait = WebDriverWait(driver, 10)
-    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, xpath_for_username )))
-    username = driver.find_element(By.XPATH, xpath_for_username)
+
+    username = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Username']")))
     username.send_keys("Admin")
-    wait.until(expected_conditions.visibility_of_element_located((By.XPATH, xpath_for_password)))
-    password = driver.find_element(By.XPATH, xpath_for_password)
+
+    password = wait.until(EC.visibility_of_element_located((By.XPATH, "//input[@placeholder='Password']")))
     password.send_keys("admin123")
-    wait.until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_for_submit)))
-    submit = driver.find_element(By.XPATH, xpath_for_submit)
+
+    submit = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
     submit.click()
+
     time.sleep(2)
     driver.quit()
-
-hrms_login_page()
